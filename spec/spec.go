fc65031a511e349d775469ed18d467c66010b138
grantseltzer <grant@capsule8.com>
2017-08-17 11:09:37 -0400

    remove large toggle switch
    Signed-off-by: grantseltzer <grant@capsule8.com>

--- spec/spec.go
+++ spec/spec.go
@@ -29,33 +29,15 @@ type FilePermission struct {
 
 type NetworkRule struct {
 	// The rule qualifiers modify network rules in the same manner that they are applied to file rules, ie. denied rules take precedence over allow rules and will both subtract permissions and quiet auditing.
-	Deny       bool
-	Permission NetworkPermission
+	Deny        bool
+	Permissions []NetworkPermission
 }
 
 //TODO: Take rest of comments from: http://wiki.apparmor.net/index.php/AppArmor_Core_Policy_Reference
 // NetworkPermission specifies what operations are allowed on the network socket
 // Should this just be a string with NetworkRules having slices of them? Validate in translation layer?
 // '-> Check other members of runtime-spec
-type NetworkPermission struct {
-	Create      bool
-	Accept      bool
-	Bind        bool
-	Connect     bool
-	Listen      bool // listen[=X] - permission to listen on a socket of the domain, type, protocl specified. If the optional backlog parameter is specified it limits the maximum size that can be pasted to listen.
-	Read        bool
-	Write       bool
-	Send        bool
-	Receive     bool
-	GetSockName bool
-	GetPeerName bool
-	GetSockOpt  bool
-	SetSockOpt  bool // set socket options
-	Fcntl       bool
-	Ioctl       bool
-	Shutdown    bool
-	GetPeerSec  bool
-}
+type NetworkPermission string
 
 type NetworkType struct {
 	//The actually set of domain names is growing and ??? should be consulted for a complete updated list
